1d8ac5c86faf6771b0550a13b1e20cdc14a30c8b
RussellLuo <luopeng.he@gmail.com>
2020-05-15 14:06:19 +0800

    Add an example

---
+++ example/README.md
@@ -0,0 +1,17 @@
+# example
+
+Start the first cron, and see the output:
+
+```bash
+$ go run main.go
+2020/05/15 13:58:37 Cron started successfully
+```
+
+Start the second cron, and see the output:
+
+```bash
+$ go run main.go
+2020/05/15 13:58:39 Cron started successfully
+```
+
+Terminate either cron, see the output.
---
+++ example/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/RussellLuo/micron/cron"
+	redislocker "github.com/RussellLuo/micron/locker/redis"
+	"github.com/go-redis/redis"
+)
+
+func main() {
+	addr := flag.String("addr", "localhost:6379", "The address of the Redis server.")
+	flag.Parse()
+
+	c := cron.New(
+		redislocker.New(redis.NewClient(&redis.Options{
+			Addr: *addr,
+		})),
+		&cron.Options{
+			LockTTL: 2 * time.Second, // Assume the maximal clock error is 2s.
+			ErrHandler: func(err error) {
+				log.Printf("err: %v", err)
+			},
+		},
+	)
+	c.Add("test", "*/5 * * * * * *", func() { // nolint:errcheck
+		log.Printf("hello")
+	})
+	c.Start()
+	log.Println("Cron started successfully")
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	<-sig
+	c.Stop()
+}
